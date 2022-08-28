2a6bf443209b96086a6fd90f0e932c772903ddf9
gnaneshgn <gnaneshgn5@gmail.com>
2022-08-28 21:57:54 +0530

    added workflow,changed error in app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 
 app=Flask(__name__)
@@ -15,24 +14,7 @@ def hello():
 
 
 
-if __name__=="__main__":
-=======
-from flask import Flask
-
-app=Flask(__name__)
-
-@app.route('/',methods=['GET'])
-def index():
-    return "Starting Ml project"
-
-# Git
-
-@app.route('/hello',methods=['GET'])
-def hello():
-    return "Starting hello"
-
 
 
 if __name__=="__main__":
->>>>>>> 5d2c3beeaa4234396adc882a043d255fd2b9066d
     app.run(debug=True,port=5050)
